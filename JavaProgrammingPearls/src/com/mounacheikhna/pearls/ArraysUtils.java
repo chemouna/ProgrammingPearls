6aff7d936088b3835b48aa25494ce31f4cb42dd7
Mouna Cheikhna <cheikhnamouna@gmail.com>
2017-03-02 07:01:30 +0100

    Add linear scanning solution.

--- JavaProgrammingPearls/src/com/mounacheikhna/pearls/ArraysUtils.java
+++ JavaProgrammingPearls/src/com/mounacheikhna/pearls/ArraysUtils.java
@@ -13,4 +13,36 @@ public class ArraysUtils {
         return max;
     }
 
+    /**
+     * @param maxSum
+     * @param v main array.
+     * @param u end index.
+     * @return the max summed up sub array as an array of integers that contains a copy of
+     * the sub vector.array
+     */
+    public static int[] getSubArray(final int maxSum, int[] v, final int u) throws SubArrayNotFoundException {
+        // N^2 complexity.
+        for (int i = u; i > 0; i--) {
+            for (int j = u; j > 0; j--) {
+                if (subArraySum(v, j, i) == maxSum)
+                    return java.util.Arrays.copyOfRange(v, j, i + 1);
+            }
+        }
+        throw new SubArrayNotFoundException();
+    }
+
+    public static int subArraySum(int[] v, final int a, final int b) {
+        int result = 0;
+        for (int i = a; i < b + 1; i++) result += v[i];
+        return result;
+    }
+
+    public static int arraySum(int[] v) {
+        int result = 0;
+        for (int i = 0; i < v.length; i++) result += v[i];
+        return result;
+    }
+
+    public static class SubArrayNotFoundException extends Exception {
+    }
 }
--- JavaProgrammingPearls/src/com/mounacheikhna/pearls/Col7SubVectors.java
+++ JavaProgrammingPearls/src/com/mounacheikhna/pearls/Col7SubVectors.java
@@ -1,8 +1,7 @@
 package com.mounacheikhna.pearls;
 
+import com.mounacheikhna.pearls.ArraysUtils.SubArrayNotFoundException;
 import com.mounacheikhna.pearls.StopWatch.StopWatchException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  * Created by m.cheikhna on 02/03/2017.
@@ -12,6 +11,7 @@ public class Col7SubVectors {
     private final int[] vector;
 
     private final String output = "Max sum of sub vector.length %s = %d with algorithm %s";
+    private int subVectorEndIndex;
 
     public Col7SubVectors(final int[] vector) {
         this.vector = vector;
@@ -26,6 +26,33 @@ public class Col7SubVectors {
         } catch (final StopWatchException ex) {
             System.out.println(ex.getMessage());
         }
+    }
+
+    public void runScanMaxSoFar() throws StopWatchException {
+        try {
+            StopWatch.start();
+            final int r = ArraysUtils.arraySum(scanMaxSoFar(this.vector));
+            final String inf = String.format("runScanMaxSoFar runtime=%d ns", StopWatch.stopNS());
+            System.out.println(String.format(output, this.vector.length, r, inf));
+        } catch (final StopWatchException | SubArrayNotFoundException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+
+    public int[] scanMaxSoFar(int[] vector) throws SubArrayNotFoundException {
+
+        // integer eI = end index of sub vector in vector, sI = start index.
+        int maxSoFar = 0, maxEndingHere = 0, tempMax = 0, eI = 0;
+
+        for (int i = 0; i < vector.length; i++) {
+            maxEndingHere = ArraysUtils.max(maxEndingHere + vector[i], 0);
+            tempMax = ArraysUtils.max(maxSoFar, maxEndingHere);
+            eI = tempMax > maxSoFar ? i : eI;
+            maxSoFar = tempMax > maxSoFar ? tempMax : maxSoFar;
+        }
+
+        this.subVectorEndIndex = eI;
+        return ArraysUtils.getSubArray(maxSoFar, vector, eI);
     }
 
     private int divideAndConquer(int[] vector, final int l, final int u) {
@@ -54,21 +81,23 @@ public class Col7SubVectors {
     }
 
     public static void main(String[] args) {
+        try {
+            Col7SubVectors subVectors = new Col7SubVectors(new int[] {
+                31, -41, 59, 26, -53, 58, 97, -93, -23, 84
+            });
+            subVectors.runDivideAndConquer();
+            subVectors.runScanMaxSoFar();
 
-        Col7SubVectors subVectors = new Col7SubVectors(new int[] {
-            31, -41, 59, 26, -53, 58, 97, -93, -23, 84
-        });
-        subVectors.runDivideAndConquer();
-
-        subVectors = new Col7SubVectors(new int[] {
-            31, -41, 59, 26, -53, 58, 97, -93, -23, 84, -43, -54, -32, -43, -11111111,
-            -543, -543, 2, 0, -543, -32, 5, 6, -54, -654, 9, 3, 5, -5432, 2, 3, 1, 0,
-            0, 0, 0, 1, 2, 3, 4, -54, 0, 0, 32, 0, 1, 2, 3, 2, 1, 2, 3, -43, 0, 0, 0,
-            -34, 100, 100, 200, -200, 300, 400, -543, 5, 54, -43, 0, 43
-        });
-        subVectors.runDivideAndConquer();
-
-
+            subVectors = new Col7SubVectors(new int[] {
+                31, -41, 59, 26, -53, 58, 97, -93, -23, 84, -43, -54, -32, -43, -11111111, -543,
+                -543, 2, 0, -543, -32, 5, 6, -54, -654, 9, 3, 5, -5432, 2, 3, 1, 0, 0, 0, 0, 1, 2,
+                3, 4, -54, 0, 0, 32, 0, 1, 2, 3, 2, 1, 2, 3, -43, 0, 0, 0, -34, 100, 100, 200, -200,
+                300, 400, -543, 5, 54, -43, 0, 43
+            });
+            subVectors.runDivideAndConquer();
+            subVectors.runScanMaxSoFar();
+        } catch (StopWatchException e) {
+            e.printStackTrace();
+        }
     }
-
 }
